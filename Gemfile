1c8867e8765a702aa35e155d1d01e3cdc991eec8
James LeDoux <ledouxja@bc.edu>
2015-09-28 15:32:34 -0400

    made database changes

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'rails', '4.1.9'
-gem 'sqlite3'
 gem 'sass-rails', '~> 5.0.4'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -14,3 +13,7 @@ gem 'spring',        group: :development
 
 gem 'devise', '~>3.5.2'
 gem 'bootstrap-sass', '~> 3.3.5.1'
+
+gem 'sqlite3'
+gem 'pg'
+gem 'taps'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150928173238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "url"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150928173238) do
     t.integer  "user_id"
   end
 
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150928173238) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
